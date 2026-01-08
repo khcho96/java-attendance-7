3f71603ccc6327fc3e34a13bef69e40c640c32ba
khcho96 <khcho1492@gmail.com>
2026-01-08 15:30:59 +0900

    first commit

--- src/main/java/attendance/Application.java
+++ src/main/java/attendance/Application.java
@@ -1,19 +1,7 @@
 package attendance;
 
-import attendance.command.MenuCommandRegistry;
-import attendance.controller.AttendanceController;
-import attendance.service.AttendanceService;
-import java.io.IOException;
-
 public class Application {
     public static void main(String[] args) {
-        AttendanceService service = new AttendanceService();
-        MenuCommandRegistry registry = MenuCommandRegistry.from(service);
-        AttendanceController controller = new AttendanceController(registry, service);
-        try {
-            controller.run();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+
     }
 }
--- src/main/java/attendance/dto/RecordQueryResult.java
+++
@@ -1,9 +0,0 @@
-package attendance.dto;
-
-import attendance.domain.Crew;
-
-public record RecordQueryResult(Crew crew) {
-    public static RecordQueryResult from(Crew crew) {
-        return new RecordQueryResult(crew);
-    }
-}
